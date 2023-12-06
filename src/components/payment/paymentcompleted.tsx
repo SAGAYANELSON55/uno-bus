436375d10fafc7ad60a1f09bea4d7254ba225c1c
SAGAYANELSON55 <sagayanelson43@gmail.com>
2023-12-06 17:01:29 +0530

    update required

--- src/components/payment/paymentcompleted.tsx
+++ src/components/payment/paymentcompleted.tsx
@@ -4,24 +4,28 @@ import { useRouter } from "next/router";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../../store/index";
 
-const Paymentcompleted: React.FC = () => {
+const PaymentCompleted: React.FC = () => {
   const busData = useSelector((state: RootState) => state.busData.busData);
   const filter = useSelector((state: RootState) => state.seatLog.pathname);
-  console.log(filter);
+  const busno = filter.split("=")[1];
+  console.log(busno);
+  const bus = busData.buses.filter((bus) => bus.busNo === busno);
+  console.log(bus);
+
   const router = useRouter();
   function homeLoader() {
     router.replace("/");
   }
 
-  // async function loadData() {
-  //   const data = fetch("api/busData/updateBusDetails",{
-  //     method: 'PATCH',
-  //     headers:{
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify()
-  //   });
-  // }
+  async function loadData() {
+    const data = fetch("api/busData/updateBusDetails", {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(bus),
+    });
+  }
   useEffect(() => {});
   return (
     <div style={{ textAlign: "center", marginTop: "250px" }}>
@@ -36,4 +40,4 @@ const Paymentcompleted: React.FC = () => {
   );
 };
 
-export default Paymentcompleted;
+export default PaymentCompleted;

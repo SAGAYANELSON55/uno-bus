3ba61be576b4d30437a8d7e1c866abfc703cb9c0
SAGAYANELSON55 <sagayanelson43@gmail.com>
2023-12-20 17:01:11 +0530

    fixed a bug

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,28 +1,38 @@
 import AuthForm from "@/components/auth/auth-form";
 import { useState, useLayoutEffect } from "react";
 import { useRouter } from "next/router";
-import { getSession } from "next-auth/react";
+import { getSession, useSession } from "next-auth/react";
 import Loader from "@/components/layout/loader";
 
 export default function Auth() {
   const [isLoading, setIsLoading] = useState(true);
   const router = useRouter();
-
+  const { data, status } = useSession();
   //redirect logic after successfull login
 
   useLayoutEffect(() => {
-    getSession().then((session) => {
-      if (session?.user.name === "Admin") {
+    // getSession().then((session) => {
+    //   console.log(session);
+    //   if (session?.user.name === "Admin") {
+    //     router.replace("/admin");
+    //   }
+    //   if (session?.user.name === "User") {
+    //     console.log(session);
+    //     router.replace("/");
+    //   } else {
+    //     setIsLoading(false);
+    //   }
+    // });
+    if (data) {
+      if (data?.user?.name === "Admin") {
         router.replace("/admin");
-      }
-      if (session?.user.name === "User") {
-        console.log(session);
-        router.replace("/");
       } else {
-        setIsLoading(false);
+        router.replace("/");
       }
-    });
-  });
+    } else {
+      setIsLoading(false);
+    }
+  }, [setIsLoading, data, router]);
 
   if (isLoading) {
     return <Loader />;
